47d27c62c1c3cfc17a497267f253710a49c16a21
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-11-03 15:18:49 +0600

    Fix issues with darkmode toggle

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,20 +25,20 @@ const menu = document.getElementById("menu");
 const cross = document.getElementById("cross");
 
 menu.addEventListener("click", () => {
-  toggleClass()
+    toggleClass()
 })
 
 cross.addEventListener("click", () => {
-  toggleClass()
+    toggleClass()
 })
 
 
 
 
 function toggleClass() {
-  var navMenu = document.getElementById("nav-menu");
-  console.log("hello")
-  navMenu.classList.toggle("nav-menu-display");
+    var navMenu = document.getElementById("nav-menu");
+    console.log("hello")
+    navMenu.classList.toggle("nav-menu-display");
 }
 
 
@@ -68,10 +68,10 @@ function addCopyButtons(clipboard) {
 
         var pre = codeBlock.parentNode;
         // if (pre.parentNode.classList.contains('highlight')) {
-            // var highlight = pre.parentNode;
-            // highlight.parentNode.insertBefore(button, highlight);
+        // var highlight = pre.parentNode;
+        // highlight.parentNode.insertBefore(button, highlight);
         // } else {
-            pre.parentNode.insertBefore(button, pre);
+        pre.parentNode.insertBefore(button, pre);
         // }
     });
 }
@@ -84,9 +84,52 @@ if (navigator && navigator.clipboard) {
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js';
     script.integrity = 'sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=';
     script.crossOrigin = 'anonymous';
-    script.onload = function() {
+    script.onload = function () {
         addCopyButtons(clipboard);
     };
 
     document.body.appendChild(script);
 }
+
+
+// dark mode
+import Darkmode from 'darkmode-js';
+
+new Darkmode().showWidget();
+// const darkmode = new Darkmode()
+// darkmode.toggle()
+
+// const moon = document.getElementById("moon")
+// const sun = document.getElementById("sun")
+// const navw = document.getElementById("nav-icon-white")
+// const navb = document.getElementById("nav-icon-black")
+
+// function mode() {
+//     if (darkmode.isActivated()) {
+//         moon.classList.toggle("mode")
+//         navb.classList.toggle("mode")
+//         navw.classList.toggle("mode")
+//       console.log("white")
+//     } else {
+//         sun.classList.toggle("mode")
+//         // navb.classList.toggle("mode")
+//         navw.classList.toggle("mode")
+//       console.log("black")
+//     }
+// }
+
+// mode()
+
+// moon.addEventListener("click", () => {
+//     darkmode.toggle()
+//   mode()
+
+
+// })
+
+// sun.addEventListener("click", () => {
+//     darkmode.toggle()
+//   mode()
+// })
+
+// console.log(darkmode.isActivated()) // will return true
